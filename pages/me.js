a07952e2b759137fb541ec9612f5e364c5dff107
mohammedlebah-arch <mohammedlebah@gmail.com>
2026-04-03 15:46:49 +0100

    Update me.js

--- pages/me.js
+++ pages/me.js
@@ -1,4 +1,4 @@
-import { requireAdmin } from '../../../lib/auth';
+import { requireAdmin } from '../lib/auth';
 
 async function handler(req, res) {
   return res.status(200).json({ user: req.user });
