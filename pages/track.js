efbc0de10522ca6e022e4efe15f4220e26eb8d56
mohammedlebah-arch <mohammedlebah@gmail.com>
2026-04-06 17:16:19 +0100

    Create track.js

---
+++ pages/track.js
@@ -0,0 +1,8 @@
+export default function TrackPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Order Tracking</h1>
+      <p>Enter your order ID to track your order.</p>
+    </div>
+  );
+}
