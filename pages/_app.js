03bcd1ec638b70d9176a4cc47d07131cd8be4d65
mohammedlebah-arch <mohammedlebah@gmail.com>
2026-04-07 07:12:28 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,17 @@
-import '../styles/globals.css';
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+
   useEffect(() => {
-    // Generate session ID for analytics
-    if (!sessionStorage.getItem('fenntel_session')) {
-      const sid = 'sess_' + Math.random().toString(36).slice(2) + Date.now().toString(36);
-      sessionStorage.setItem('fenntel_session', sid);
-    }
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/eruda";
+    script.onload = () => {
+      window.eruda.init();
+    };
+    document.body.appendChild(script);
   }, []);
 
-  return (
-    <>
-      <canvas id="particle-canvas" />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
